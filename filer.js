6be8043db97c178306e89e7a98816747cd51faae
Jeremyzones <jeremzy.jones@gmail.com>
2017-07-05 12:18:22 -0500

    Moved directory check and creation above sorting code.

--- filer.js
+++ filer.js
@@ -8,30 +8,6 @@ const downloads = home + "/Downloads"
 
 const FILE_TYPES = JSON.parse(fs.readFileSync("extensions.json"))
 
-
-
-fs.readdir(downloads, (err, items) => {
-
-    for(var item in items) {
-        item = downloads + "/" + items[item]
-        itemExtension = path.extname(item)
-        
-        if(itemExtension) {
-            for(var type in FILE_TYPES) {
-                console.log(FILE_TYPES[type])
-                //type = downloads + "/" + type
-                
-                if(itemExtension.slice(1) in FILE_TYPES[type]) {
-                    mv(item, type + "/" + item, (err) => {
-                        console.log(err)
-                    })
-                }
-            }
-        }
-        
-    }
-})
-
 // Check if file type directory exists. Create if it doesn't.
 for(var type in FILE_TYPES) {
     //console.log(file)
@@ -40,3 +16,25 @@ for(var type in FILE_TYPES) {
         fs.mkdirSync(type)
     }
 }
+
+fs.readdir(downloads, (err, items) => {
+
+    for(var item in items) {
+        item = downloads + "/" + items[item]
+        itemExtension = path.extname(item)
+
+        if(itemExtension) {
+            for(var type in FILE_TYPES) {
+                console.log(FILE_TYPES[type])
+                //type = downloads + "/" + type
+
+                if(itemExtension.slice(1) in FILE_TYPES[type]) {
+                    mv(item, type + "/" + item, (err) => {
+                        console.log(err)
+                    })
+                }
+            }
+        }
+
+    }
+})
